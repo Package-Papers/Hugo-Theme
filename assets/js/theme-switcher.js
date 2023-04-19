81469175931882534f36776a3a64db51988232ff
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-19 16:56:31 +0700

    Refactored darkmode logic

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,18 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const body = document.body;
-  const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+const toggleButton = document.getElementById('theme-toggle');
 
-  // Toggle theme on button click
-  toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark');
-    const currentTheme = body.classList.contains('dark') ? 'dark' : 'light';
-    localStorage.setItem('theme', currentTheme);
-  });
+const setTheme = (theme) => {
+  body.classList.toggle('dark', theme === 'dark');
+  localStorage.setItem('theme', theme);
+};
 
-  // Apply saved theme on page load
-  const savedTheme = localStorage.getItem('theme');
-
-  if (savedTheme) {
-    body.classList.toggle('dark', savedTheme === 'dark');
-  }
+// Toggle theme on button click
+toggleButton.addEventListener('click', () => {
+  const currentTheme = body.classList.contains('dark') ? 'light' : 'dark';
+  setTheme(currentTheme);
 });
+
+// Apply saved theme on page load
+const savedTheme = localStorage.getItem('theme');
+setTheme(savedTheme || 'light');
