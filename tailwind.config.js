edbbba9c7facceb5d27d5fbf81202bfc71e6327e
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-19 17:49:34 +0700

    refactored and cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["content/**/*.md", "layouts/**/*.html"],
+  content: ["content/**/*.md", "layouts/**/*.html", "assets/**/*.js"],
   darkMode: "class",
   theme: {
     extend: {},
