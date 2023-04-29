1888b5408ab0c24134afd0490204d858d386e8ff
Austin Maddison <Austin.madison10152@gmail.com>
2023-04-29 15:51:14 +0700

    Commit Test

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["content/**/*.md", "layouts/**/*.html", "assets/**/*.js"],
   darkMode: "class",
   theme: {
+    colors: {
+
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [
