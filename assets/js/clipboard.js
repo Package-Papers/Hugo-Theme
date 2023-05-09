424f5862ac3deb18279187a496300dcf9ebda259
Alexandre Simon <aleandre6579.simon@gmail.com>
2023-05-09 21:16:00 +0700

    Modifying author page layout

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -1,23 +1,11 @@
-document.addEventListener("mouseover", function(e){
-    if(e.target.id == "social") {
-        //copyToClipboard(e.target);
-        console.log(e.target.children[1])
+document.addEventListener("mousedown", function(e){
+    if(e.target.classList.contains("social")) {
+        copyToClipboard(e.target.nextSibling.nextSibling.textContent);
     }
 });
 
-function copyToClipboard(socialSVG) {
-    // Get the text field
-    var copyText = socialSVG.firstChild;
-  
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
+function copyToClipboard(textToCopy) {
      // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-    console.log(copyText);
-    console.log(copyText.value);
-  
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
-  }
+    navigator.clipboard.writeText(textToCopy);
+    console.log(textToCopy);
+}
